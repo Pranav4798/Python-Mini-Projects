24ad522a0a570df86d452774bc6918eec222b97c
Shiro <prnvyt07@gmail.com>
2025-05-05 12:47:41 +0530

    Started working on it

---
+++ RecipeApp/RecipeApp_v1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+Title = input('Enter the name of the Recipe: ')
+Ingredients =  input('Enter the list of ingredients: ')
+Steps = input('Enter the steps: ')
+
+print(f'Saved Recipe {Title}!')
